9aafd2e4533c14a5476a5b1f3ca25be416737a6e
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-05-12 21:11:52 -0700

    trial tests

--- src/test/java/TrialTests.java
+++ src/test/java/TrialTests.java
@@ -36,7 +36,6 @@ public class TrialTests extends BaseTest {
     @Test
     public void testVitaliiArtemenkoFlukeOpenProductsPage() {
         openProductsPage();
-
         Assert.assertEquals(PRODUCTS_PAGE_TITLE, getDriver().getTitle());
         Assert.assertEquals(PRODUCTS_PAGE_HEADER, getDriver().findElement(By.xpath(HEADER_XPATH)).getText());
     }
@@ -44,7 +43,6 @@ public class TrialTests extends BaseTest {
     @Test
     public void testVitaliiArtemenkoFlukeOpenFeaturedPage() {
         openFeaturedPage();
-
         Assert.assertEquals(FEATURED_PAGE_TITLE, getDriver().getTitle());
         Assert.assertEquals(FEATURE_PAGE_HEADER, getDriver().findElement(By.xpath(HEADER_XPATH)).getText());
     }
@@ -52,7 +50,6 @@ public class TrialTests extends BaseTest {
     @Test
     public void testVitaliiArtemenkoFlukeOpenAwardsPage() {
         openAwardsPage();
-
         Assert.assertEquals(AWARDS_PAGE_TITLE, getDriver().getTitle());
         Assert.assertEquals(AWARDS_PAGE_HEADER, getDriver().findElement(By.xpath(AWARDS_HEADER_XPATH)).getText());
     }
